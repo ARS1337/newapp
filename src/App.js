da5236d8fd919dc147792bc8a26be2593ed7529e
abhaySingh <38357472+ARS1337@users.noreply.github.com>
2022-05-26 19:44:32 +0530

    spacing correction

--- src/App.js
+++ src/App.js
@@ -30,22 +30,42 @@ function App() {
       <div className="flex items-center justify-center ">
         <Header poleDate={poleDate} setcurrPage={setcurrPage} />
       </div>
-      <div className="w-full" >
+      <div className="w-full">
         {/* Hero section start */}
-        {currPage.includes('main') ? <HeroSection /> : ""}
+        {currPage.includes("main") ? (
+          <>
+            <HeroSection />
+            <div className=" flex items-center justify-center ">
+              <div className="w-full text-center  flex items-center justify-center flex-col my-12">
+                <label className="mb-6 text-4xl font-Poppins-SemiBold">Propel Your Career to the next level</label>
+                <label className="text-center mb-6 text-gray-500 text-lg md:px-36">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices dolor non erat sollicitudin, in
+                  pretium ante varius. Cras lorem diam, sollicitudin eget tortor vitae, sodales faucibus massa. Nullam
+                  iaculis luctus metus, non interdum nulla commodo ac.
+                </label>
+                <a href="/" className="flex flex-row items-center text-blue-custom font-Poppins-Bold text-lg ">
+                  <div>View more </div>
+                  <img src="/assets/arrow-right.png" className="pl-3 h-4 w-auto" alt="view more" />
+                </a>
+              </div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
         {/* Hero section end */}
 
         {/* table starts */}
         <div className="bg-light-gray-custom flex flex-col lg:flex-row items-start justify-center w-full px-4 md:px-4 lg:px-12">
-          <div className="  flex flex-col items-start justify-center w-full md:9/12  lg:w-8/12 xl:6/12">
+          <div className="  flex flex-col items-start justify-center w-full md:9/12  lg:w-[60%] xl:6/12">
             <Routes>
-              <Route path="/" element={<MainPage setcurrPage={setcurrPage}/>} />
-              <Route path="Second" element={<SecondPageUniversityDetails  setcurrPage={setcurrPage}/>} />
-              <Route path="Third" element={<ThirdPage setcurrPage={setcurrPage}/>} />
+              <Route path="/" element={<MainPage setcurrPage={setcurrPage} />} />
+              <Route path="Second" element={<SecondPageUniversityDetails setcurrPage={setcurrPage} />} />
+              <Route path="Third" element={<ThirdPage setcurrPage={setcurrPage} />} />
             </Routes>
             <HelpSection questionList={data.helpQuestionList} />
             <FaqSection />
-            <TestimonialContainer autoScroll={true}  key="testimonial" heading={true} id="">
+            <TestimonialContainer autoScroll={true} key="testimonial" heading={true} id="">
               {data.testimonials.map((testimonial, key) => {
                 return (
                   <div key={key} className="mx-4 w-[300px] lg:w-[600px]  flex items-center justify-center">
@@ -57,7 +77,7 @@ function App() {
             <DisclaimerComponent />
           </div>
           {/* right container */}
-          <div className="mt-28 font-Poppins-Medium lg:ml-4  justify-center flex w-full md:3/12 lg:w-3/12 xl:2/12 flex-col">
+          <div className="mt-28 font-Poppins-Medium lg:ml-12  justify-center flex w-full md:3/12 lg:w-[20%] xl:2/12 flex-col">
             {/* 1st ad */}
             <Ad1 />
             {/* 2nd ad */}
